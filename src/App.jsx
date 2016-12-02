aaaf7dc6ed69ac4a3776063451eb9849870e33aa
Richard Dowsett <richarddowsett@gmail.com>
2016-12-02 22:48:22 +0000

    move filterbar to redux and use store classes for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import {connect} from 'react-redux'
 import {toggleCategory, toggleActivity} from './ActionTypes'
 import {createLoadAllClasses} from './ActionTypes'
 import ResultsTabsComponent from './ResultsTabComponent'
+import FilterBarComponent from './FilterBarComponent'
 
 
 
@@ -54,9 +55,7 @@ var classes = [
   }
 ]
 
-function unique(value, index, self) {
-  return self.indexOf(value) === index;
-}
+
 
 class App extends Component {
   constructor(props, context) {
@@ -94,18 +93,11 @@ class App extends Component {
 class Content extends React.Component {
   constructor(props, context) {
     super(props, context);
-    var tempCategories = []
-    var tempActivities = []
-    this.props.classes.forEach(function(c) {
-      tempCategories.push(c.category)
-      tempActivities.push(c.activity)
-    })
+
     this.state = {
       searchCategories: [],
       searchActivities: [],
       location: '',
-      allCategories: tempCategories.filter(unique),
-      allActivities: tempActivities.filter(unique),
       filterOpen: false
     }
     this.handleCategoryChange = this.handleCategoryChange.bind(this)
@@ -138,8 +130,8 @@ class Content extends React.Component {
           </Col>
         </Row>
         <ReactCollapse isOpened={this.state.filterOpen}>
-          <SearchForm allCategories={this.state.allCategories} allActivities={this.state.allActivities} locationValue={this.state.location} onCategoryChange={this.handleCategoryChange} onActivityChange={this.handleActivityChange}/>
-        </ReactCollapse>
+<FilterBarComponent/>
+          </ReactCollapse>
         <Row className="show-grid">
 
 
@@ -150,113 +142,7 @@ class Content extends React.Component {
   }
 }
 
-class SearchForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this);
-    this.handleActivityChange = this.handleActivityChange.bind(this);
-  }
 
-  handleChange(e) {
-    console.log('handle searchform change' + e)
-    this.props.onCategoryChange(e)
-  }
-
-  handleActivityChange(e) {
-    console.log('handle activity change' + e)
-    this.props.onActivityChange(e)
-  }
-
-  handleLocationChange(e) {
-    console.log('location change')
-  }
-
-  render() {
-    var categoryButtonList = []
-    function createButton(changeFunc, category) {
-      console.log("category -> " + category + " -> " + "changeFunc" + changeFunc)
-      categoryButtonList.push(< FilterButton key = {
-        category
-      }
-      text = {
-        category
-      }
-      buttonClick = {
-        changeFunc
-      } />)
-    }
-    this.props.allCategories.forEach(createButton.bind(null, this.handleChange))
-    var activityButtonList = []
-    function createActivityButton(changeFunc, activity) {
-      console.log("activity -> " + activity + " -> " + "changeFunc" + changeFunc)
-      activityButtonList.push(< FilterButton key = {
-        activity
-      }
-      text = {
-        activity
-      }
-      buttonClick = {
-        changeFunc
-      } />)
-    }
-    this.props.allActivities.forEach(createActivityButton.bind(null, this.handleActivityChange))
-
-    return (
-      <Row className="show-grid">
-        <Col lg={8} md={8}>
-          <Accordion>
-            <Panel header="Categories" eventKey="1">
-              <ButtonGroup>
-                {categoryButtonList}
-              </ButtonGroup>
-            </Panel>
-            <Panel header="Activity" eventKey="2">
-              <ButtonGroup>
-                {activityButtonList}
-              </ButtonGroup>
-            </Panel>
-          </Accordion>
-        </Col>
-        <Col lg={4} md={4}>
-          <Panel header="Days">
-            blah
-          </Panel>
-        </Col>
-      </Row>
-    )
-  }
-}
-
-class FilterButton extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: true,
-      style: "primary"
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange() {
-    if (this.state.active) {
-      this.setState({active: false, style: "info"})
-    } else {
-      this.setState({active: true, style: "primary"})
-    }
-    this.props.buttonClick(this.props.text)
-  }
-
-  render() {
-    return (
-      <Button onClick={this.handleChange}>{this.props.text}</Button>
-    )
-  }
-
-}
-
-FilterButton.propTypes = {
-  buttonClick: PropTypes.func.isRequired
-}
 
 class AppJumbotron extends Component {
   render() {
